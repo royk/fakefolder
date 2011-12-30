2ed1120f7564a5637e029ba9a95fc6eb3a0a6249
RoyK <roeiklein@gmail.com>
2011-12-30 18:49:06 +0200

    Back now works. Added missing project files

--- src/main.js
+++ src/main.js
@@ -1,129 +1,138 @@
 var fakeFolder = {
-        depths:{},
-        prevDepths:{},
-        xmlDoc:undefined,
-        xmlLoaded:false,
-        currentDepth:0,
-        importXML:function(xmlfile)
+    depths:{},
+    backStack:new Array(),
+    xmlDoc:undefined,
+    xmlLoaded:false,
+    currentGroup:undefined,
+    currentDepth:0,
+    importXML:function(xmlfile)
+    {
+        try
         {
-            try
-            {
-                var xmlhttp = new XMLHttpRequest();
-                xmlhttp.open("GET", xmlfile, false);
-            }
-            catch (Exception)
-            {
-                var ie = (typeof window.ActiveXObject != 'undefined');
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", xmlfile, false);
+        }
+        catch (Exception)
+        {
+            var ie = (typeof window.ActiveXObject != 'undefined');
 
-                if (ie)
-                {
-                    this.xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-                    this.xmlDoc.async = false;
-                    while(this.xmlDoc.readyState != 4) {};
-                    this.xmlDoc.load(xmlfile);
-                    this.xmlloaded = true;
-                    this.parseXML();
-                }
-                else
-                {
-                    this.xmlDoc = document.implementation.createDocument("", "", null);
-                    this.xmlDoc.onload = readXML;
-                    this.xmlDoc.load(xmlfile);
-                    this.xmlloaded = true;
-                    this.parseXML();
-                }
-            }
-
-            if (this.xmlLoaded===false)
+            if (ie)
             {
-                xmlhttp.setRequestHeader('Content-Type', 'text/xml')
-                xmlhttp.send("");
-                this.xmlDoc = xmlhttp.responseXML;
-                this.xmlLoaded = true;
+                this.xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+                this.xmlDoc.async = false;
+                while(this.xmlDoc.readyState != 4) {};
+                this.xmlDoc.load(xmlfile);
+                this.xmlloaded = true;
                 this.parseXML();
             }
-        },
+            else
+            {
+                this.xmlDoc = document.implementation.createDocument("", "", null);
+                this.xmlDoc.onload = readXML;
+                this.xmlDoc.load(xmlfile);
+                this.xmlloaded = true;
+                this.parseXML();
+            }
+        }
 
-        parseXML:function(){
-                 console.log($(this.xmlDoc).find("depth") )
-                $(this.xmlDoc).find("children").each(function()
-                {
-                    var depth = $(this).attr("depth");
-                    var depthArr = fakeFolder.depths["depth"+depth];
-                    if (depthArr==undefined){
-                        fakeFolder.depths["depth"+depth] = new Array();
-                        depthArr = fakeFolder.depths["depth"+depth];
+        if (this.xmlLoaded===false)
+        {
+            xmlhttp.setRequestHeader('Content-Type', 'text/xml')
+            xmlhttp.send("");
+            this.xmlDoc = xmlhttp.responseXML;
+            this.xmlLoaded = true;
+            this.parseXML();
+        }
+    },
+
+    parseXML:function(){
+        console.log($(this.xmlDoc).find("depth") )
+        $(this.xmlDoc).find("children").each(function()
+        {
+            var depth = $(this).attr("depth");
+            var depthArr = fakeFolder.depths["depth"+depth];
+            if (depthArr==undefined){
+                fakeFolder.depths["depth"+depth] = new Array();
+                depthArr = fakeFolder.depths["depth"+depth];
+            }
+            var items = {arr:new Array(), viewed:false}
+            $(this).find("item").each(function(){
+                var o = $(this).attr("title");
+                items.arr.push(o);
+            })
+            depthArr.push(items);
+        })
+        this.loadNextDepth();
+
+    },
+
+    loadPreviousDepth:function(){
+        if (this.backStack.length>0){
+            var group = this.backStack.pop();
+            this.loadGroup(group);
+        }
+    },
+    loadNextDepth:function(groupNumber){
+
+        var depth = this.depths["depth"+this.currentDepth];
+        var group;
+        var done = false;
+        var count = 0;
+        console.log("loading depth "+this.currentDepth);
+        if (depth){
+            if (groupNumber===undefined){
+                groupNumber = Math.floor(Math.random()*depth.length);
+
+                while (done===false){
+                    group = depth[groupNumber]
+                    if (group && group.viewed===false){
+                        done = true;
+                        break;
                     }
-                    var items = {arr:new Array(), viewed:false}
-                    $(this).find("item").each(function(){
-                        var o = $(this).attr("title");
-                        items.arr.push(o);
-                    })
-                    depthArr.push(items);
-                })
-                this.loadNextDepth();
+                    groupNumber++;
+                    groupNumber = groupNumber%depth.length;
+                    count++;
+                    if (count===depth.length){
+                        done = true;
+                        this.currentDepth++;
+                        this.loadNextDepth();
+                        return;
+                    }
+                }
+            }
+            if (this.currentGroup!=undefined){
+                this. backStack.push(this.currentGroup);
+            }
+            this.loadGroup(group);
+        }
 
-        },
 
-        loadPreviousDepth:function(){
-              this.currentDepth-=2;
-              if (this.currentDepth<0){
-                  this.currentDepth = 0;
-              }
-              this.loadNextDepth(this.prevDepths[this.currentDepth]);
-          },
-            loadNextDepth:function(groupNumber){
+    },
 
-              var depth = this.depths["depth"+this.currentDepth];
-              var group;
-              var done = false;
-              var count = 0;
-              console.log("loading depth "+this.currentDepth);
-              if (depth){
-                  if (groupNumber===undefined){
-                      groupNumber = Math.floor(Math.random()*depth.length);
+    loadGroup:function(group){
+        if (group){
+            this. currentGroup = group;
+            group.viewed = true;
+            this.clearTable();
+            console.log($("#linkTable"));
+            $("#linkTable").delay(500).queue(function(){
+                console.log("creating depth "+this.currentDepth);
+                for each (o in group.arr){
+                    fakeFolder.appendTable(o, "javascript:fakeFolder.loadNextDepth()");
+                    $("#linkTable").dequeue();
+                }
+            });
+        }
 
-                      while (done===false){
-                          group = depth[groupNumber]
-                          if (group && group.viewed===false){
-                              done = true;
-                              break;
-                          }
-                          groupNumber++;
-                          groupNumber = groupNumber%depth.length;
-                          count++;
-                          if (count===depth.length){
-                              done = true;
-                              this.currentDepth++;
-                              this.loadNextDepth();
-                              return;
-                          }
-                      }
-                  }
-                  if (group){
-                      group.viewed = true;
-                      this.clearTable();
-                      this.prevDepths[this.currentDepth] =  groupNumber;
-                      console.log($("#linkTable"));
-                      $("#linkTable").delay(500).queue(function(){
-                          console.log("creating depth "+this.currentDepth);
-                          for each (o in group.arr){
-                              fakeFolder.appendTable(o, "javascript:fakeFolder.loadNextDepth()");
-                              $("#linkTable").dequeue();
-                          }
-                      });
-                  }
-              }
+    },
 
-          },
+    appendTable:function(title, url){
+        var code = '<tr ><td><img src="assets/blank.gif"><img src="assets/folder.jpg" alt="[   ]"> <a href="'+url+'">'+title+'</a></td></tr>';
+        $("#linkTable > tbody:last").append(code);;
+    },
 
-        appendTable:function(title, url){
-            var code = '<tr ><td><img src="assets/blank.gif"><img src="assets/folder.jpg" alt="[   ]"> <a href="'+url+'">'+title+'</a></td></tr>';
-            $("#linkTable > tbody:last").append(code);;
-        },
-
-        clearTable:function(){
-             $("#linkTable tr").remove().fadeOut(500);
-         }
+    clearTable:function(){
+        $("#linkTable tr").remove().fadeOut(500);
+    }
 
 };
